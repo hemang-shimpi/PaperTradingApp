11db3bd9944acc2c2645ee9f3cdc79f6df20a07b
daredevilx616 <abdullahnasirpti@gmail.com>
2025-03-05 15:00:54 -0500

    write function-csv

--- utils/ingestUtility.py
+++ utils/ingestUtility.py
@@ -24,3 +24,8 @@ def renameOutputFile(OutputPath):
     csv_file = [x.path for x in files if x.path.endswith(".csv")][0]
     dbutils.fs.mv(csv_file, OutputPath.rstrip('/') + ".csv")
     dbutils.fs.rm(OutputPath, recurse=True)
+
+def writeToDataLake(df, OutputPathFile, Mode):
+    df.coalesce(1).write.format('com.databricks.spark.csv').mode(Mode).option("header", "true").option("delimiter",
+                                                                                                       ",").save(
+        OutputPathFile)
